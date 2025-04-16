c597ef4b87e9fee86b6c122a60c53e311554b07e
Dhruv <dhruvkulsh@gmail.com>
2025-04-16 21:36:10 +0530

    conutry pages added

--- src/components/Layout/CountryDetails.jsx
+++ src/components/Layout/CountryDetails.jsx
@@ -5,7 +5,7 @@ export const CountryDetails = () => {
     const params = useParams();
     console.log(params);
 
-    const [country, setCountry] = useState([]);
+    const [country, setCountry] = useState();
     const [loader, setLoader] = useState(true);
 
     const getApiData = async () => {
@@ -13,10 +13,10 @@ export const CountryDetails = () => {
             // https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags -- TO get all country data
             // https://restcountries.com/v3.1/name/Grenada?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags
 
-            const res = await fetch(`https://restcountries.com/v3.1/name/${params}?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags`);
+            const res = await fetch(`https://restcountries.com/v3.1/name/${params.id}?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags`);
             const data = await res.json();
-            console.log(`The data is `, data);
-            setCountry(data);
+            console.log(`The data is `, data[0]);
+            setCountry(data[0]);
             setLoader(false);
             return data;
         }
@@ -27,8 +27,13 @@ export const CountryDetails = () => {
     }
 
     useEffect(() => {
-        const promise = getApiData();
+        const promise = getApiData();        
     }, []);
+
+    // useEffect(() => {
+    //     if(country)
+    //     console.log(`this ${Object.keys(country.name.nativeName)}`);
+    // }, [country])
 
     // console.log(allCountryData);
 
@@ -38,19 +43,64 @@ export const CountryDetails = () => {
     }
 
 
-    return (
-        <>
-            {
-                country.map((indiCount, key) => {
-                    return (
-                        <ul key={key}>
-                            <li>{indiCount.name.common}</li>
-                        </ul>
-                    )
-                })
-            }
 
-        </>
+    return (
+        <section className="card country-details-card container">
+            <div className="container-card bg-white-box">
+                <div className="country-image grid grid-two-cols">
+                    <img src={country.flags.svg} alt={country.flags.alt} className="flag" />
+                </div>
+                <div className="country-content">
+                    <p className="card-tile">{country.name.official}</p>
+                    <div className="info-container">
+                        <p>
+                            <span className="card-description">Native Names: </span>
+                            {
+                                Object.keys(country.name.nativeName).map((key) => {
+                                    return country.name.nativeName[key].common
+                                }).join(", ")
+                            }
+                        </p>
+                        <p>
+                            <span className="card-description">Population:</span>
+                            {country.population.toLocaleString()}
+                        </p>
+                        <p>
+                            <span className="card-description">Region:</span>
+                            {country.region}
+                        </p>
+                        <p>
+                            <span className="card-description">Sub Region:</span>
+                            {country.subregion}
+                        </p>
+                        <p>
+                            <span className="card-description">Sub Region:</span>
+                            {country.capital[0]}
+                        </p>
+                        <p>
+                            <span className="card-description">Top Level Domain:</span>
+                            {country.tld[0]}
+                        </p>
+                        <p>
+                            <span className="card-description">Currencies:</span>
+                            {/* {country.currencies.INR.name}  Will be applicable if only 1 currency is there */}
+                            {Object.keys(country.currencies).map((currElem) =>{
+                                return country.currencies[currElem].name
+                            })}
+                        </p>
+                        <p>
+                            <span className="card-description">Languages:</span>
+                            {
+                            Object.keys(country.languages).map((currElem) =>{
+                                return country.languages[currElem]
+                            }).join(", ")
+                            }
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
+
 
 }
