f62b125df330ec77760ae53671bf114922605bb3
Dhruv <dhruvkulsh@gmail.com>
2025-04-16 11:36:56 +0530

    country-inital-code

--- src/components/Layout/CountryDetails.jsx
+++ src/components/Layout/CountryDetails.jsx
@@ -1,40 +1,56 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
 
 export const CountryDetails = () => {
     const params = useParams();
     console.log(params);
 
-    const [countries, setCountries] = useState([]);
-        const [loader, setLoader] = useState(true);
-    
-        const getApiData = async () => {
-            try {
-                const res = await fetch("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags");
-                const data = await res.json();
-                console.log(`The data is `, data);
-                setCountries(data);
-                setLoader(false);
-                return data; // Not returning countries as it would not be updated
-            }
-            catch (error) {
-                console.log(error);
-            }
-    
-        }
-    
-        useEffect(() => {
-            const promise = getApiData();
-            // console.log(" Promise:- ", promise);
-        }, []);
-    
-        // console.log(allCountryData);
-    
-        if (loader) {
-            return <h1 className="loader">Loading...</h1>
-    
-        }
-    
+    const [country, setCountry] = useState([]);
+    const [loader, setLoader] = useState(true);
 
-    return <h1>Heloo detsial</h1>
-    
+    const getApiData = async () => {
+        try {
+            // https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags -- TO get all country data
+            // https://restcountries.com/v3.1/name/Grenada?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags
+
+            const res = await fetch(`https://restcountries.com/v3.1/name/${params}?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags`);
+            const data = await res.json();
+            console.log(`The data is `, data);
+            setCountry(data);
+            setLoader(false);
+            return data;
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    useEffect(() => {
+        const promise = getApiData();
+    }, []);
+
+    // console.log(allCountryData);
+
+    if (loader) {
+        return <h1 className="loader">Loading...</h1>
+
+    }
+
+
+    return (
+        <>
+            {
+                country.map((indiCount, key) => {
+                    return (
+                        <ul key={key}>
+                            <li>{indiCount.name.common}</li>
+                        </ul>
+                    )
+                })
+            }
+
+        </>
+    )
+
 }
